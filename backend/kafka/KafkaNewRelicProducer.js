5fb3201655273be6aaef564e62588949ad7a745f
I549353 <kushal.bhattacharya@sap.com>
2024-09-24 00:00:37 +0530

    basic wrapper of KafkaProducer using newrelic segment for instrumenting

--- backend/kafka/KafkaNewRelicProducer.js
+++ backend/kafka/KafkaNewRelicProducer.js
@@ -3,27 +3,35 @@ const newRelic = require("newrelic");
 const logger = require("../logger/logger");
 
 class KafkaNewRelicProducer {
+  constructor() {}
 
-    constructor() {}
-    
-    async produce(topic, event, options) {
-        try {
-     //   const parentTransaction = newRelic.getTransaction();
-     //   parentTransaction.end();
-      //  const transactionName = "kafkaProducerTransactionProduce";
-     //  const result=  await newRelic.startSegment(transactionName, true, async function () {
-      //  const transaction = newRelic.getTransaction();
-        const kafkaProducer = KafkaProducer.getInstance();
-        logger.debug("Producing message to topic within segment", topic, "with correlation ID", options.correlationId);
-        await kafkaProducer.produce(topic, event, options);
-       // transaction.end();
-     //  });
-       // console.log("new relic result segment", result);
-        } catch (error) {
-        console.error("Error producing message from KafkaNewRelicProducer class:", error.message);
+  async produce(topic, event, options) {
+    try {
+      const transactionName = "kafkaProducerTransactionProduce";
+      const result = await newRelic.startSegment(
+        transactionName,
+        false,
+        async function () {
+          //  const transaction = newRelic.getTransaction();
+          const kafkaProducer = KafkaProducer.getInstance();
+          logger.debug(
+            "Producing message to topic within segment",
+            topic,
+            "with correlation ID",
+            options.correlationId
+          );
+          await kafkaProducer.produce(topic, event, options);
+          // transaction.end();
         }
+      );
+      console.log("new relic result segment", result);
+    } catch (error) {
+      console.error(
+        "Error producing message from KafkaNewRelicProducer class:",
+        error.message
+      );
     }
+  }
 }
-
 
 module.exports = KafkaNewRelicProducer;
